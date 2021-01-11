d5c2d01761dc3e5cd5924cea731dae191eba324c
Xiaoming Ye <yexiaoming@rd.netease.com>
2021-01-11 11:17:32 +0800

    MOD: 更新

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -65,6 +65,7 @@ const pageObj = {
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+      console.log(app.globalData.userInfo)
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -98,6 +99,12 @@ const pageObj = {
   goPost: function() {
     wx.navigateTo({
       url: '../post/post'
+    })
+  },
+ toPersonal() {
+    const user = this.data.userInfo.nickName
+    wx.navigateTo({
+      url: `../personal/personal?user=${user}`
     })
   }
 }
